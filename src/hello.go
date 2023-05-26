c3811a59de260599a421a93faa290e309137d1c8
Jabison Rafael <jabsr@usp.br>
2023-05-26 09:56:58 -0300

    update

---
+++ src/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Jabison"
+	var idade int = 23
+	var altura float32 = 1.86
+	fmt.Println("Hello, World!;")
+	fmt.Println("Meu nome é", nome)
+	fmt.Println("Minha idade é", idade)
+	fmt.Println("Minha altura é", altura)
+}
